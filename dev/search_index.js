8c8abe9fa54462ad25c64cea10d82d25b905ac3c
Documenter.jl <documenter@juliadocs.github.io>
2022-05-08 18:45:47 +0000

    build based on 3495c7f

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = OptiX","category":"page"},{"location":"#OptiX","page":"Home","title":"OptiX","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for OptiX.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [OptiX]","category":"page"}]
+}
