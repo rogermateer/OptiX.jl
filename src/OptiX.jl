2e151ff8b4ee31a3ce95277bfc6a244eeeeb3aae
Roger Mateer <rogermateer@gmail.com>
2022-06-06 11:04:43 +0200

    Import and internalize AlphaVantage TIME_SERIES_DAILY data

--- src/OptiX.jl
+++ src/OptiX.jl
@@ -1,5 +1,90 @@
 module OptiX
+using Reexport
+@reexport using GZip
+@reexport using JSON3
+@reexport using Dates
+import StructTypes
 
-# Write your package code here.
+"""
 
+Find all relevant files in my data lake
+
+"""
+function findFiles(dataLakePath::String,vendor::String,call::String,ticker::String)::Vector{String}
+    allFiles = []
+    for (root, dirs, files) in walkdir(dataLakePath)
+        for file in files
+            push!(allFiles,joinpath(root,file))
+        end
+    end
+    return filter(x->occursin("$(vendor).$(call).$(ticker)",x),allFiles)
 end
+export findFiles
+
+"""
+
+Read the contents of a gzipped json file into a JSON3 Object
+
+"""
+function readGZippedJson(filePath::String)::JSON3.Object
+    return GZip.open(filePath) do io
+        JSON3.read(io)
+    end
+end
+export readGZippedJson
+
+"""
+
+Internal representation of generic time series data
+
+"""
+mutable struct OhlcvBar
+    timestamp::DateTime
+    open::Float64
+    high::Float64
+    low::Float64
+    close::Float64
+    volume::Int64
+    OhlcvBar() = new()
+end
+export OhlcvBar
+
+StructTypes.StructType(::Type{OhlcvBar}) = begin
+    StructTypes.Mutable()
+end
+
+"""
+
+Convert an AlphaVantage TIME_SERIES_DAILY bar into our generic
+internal form
+
+"""
+function convertAlphaVantageTimeSeriesDailyBar(key::Symbol,value::JSON3.Object)::OhlcvBar
+    return JSON3.read("""{
+    "timestamp":"$(key)",
+    "open":$(value["1. open"]),
+    "high":$(value["2. high"]),
+    "low":$(value["3. low"]),
+    "close":$(value["4. close"]),
+    "volume":$(value["5. volume"])
+}""",OhlcvBar)
+end
+export convertAlphaVantageTimeSeriesDailyBar
+
+"""
+
+Convert all bars of an AlphaVantage TIME_SERIES_DAILY call into a
+Vector of bars of our generic internal form
+
+"""
+function convertAlphaVantageTimeSeriesDaily(json::JSON3.Object)::Vector{OhlcvBar}
+    bars = OhlcvBar[]
+    data = json["Time Series (Daily)"]
+    for key in sort(collect(keys(data)))
+        push!(bars,convertAlphaVantageTimeSeriesDailyBar(key,data[key]))
+    end
+    return bars
+end
+export convertAlphaVantageTimeSeriesDaily
+
+end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,35 @@
 using OptiX
 using Test
+using TestSetExtensions
 
-@testset "OptiX.jl" begin
-    # Write your tests here.
+#=
+@testset ExtendedTestSet "JSON3 StructTypes type generation" begin
+    JSON3.writetypes("../data/2022/05/27/AlphaVantage.TIME_SERIES_DAILY.AAPL.2022-05-27.json",
+                     "../AlphaVantageTimeSeriesDailyTypes.jl";
+                     module_name=:AlphaVantageTimeSeriesDailyTypes,
+                     root_name=:AlphaVantageTimeSeriesDaily)
+
+    # FIXME: unfortunately, AlphaVantage JSON uses timestamps as keys,
+    # which, AFAIK, JSON3 type generation doesn't handle (too
+    # easily/at all)
+end
+=#
+
+exampleFile = "../data/2022/05/27/AlphaVantage.TIME_SERIES_DAILY.AAPL.2022-05-27.json.gz"
+
+@testset ExtendedTestSet "Read a GZipped JSON file" begin
+    json = readGZippedJson(exampleFile)
+    @test (length(json["Time Series (Daily)"])) == 5680
+end
+
+@testset ExtendedTestSet "Find all relevant files" begin
+    relevantFiles = findFiles("../data","AlphaVantage","TIME_SERIES_DAILY","AAPL");
+    @test length(relevantFiles) == 746 # this is going to change day-by-day
+    @test exampleFile == relevantFiles[737]
+end
+
+@testset ExtendedTestSet "Convert AlphaVantage TIME_SERIES_DAILY file into Vector{OhlcvBar}" begin
+    json = readGZippedJson(exampleFile)
+    bars = convertAlphaVantageTimeSeriesDaily(json)
+    @test length(bars) == 5680
 end
